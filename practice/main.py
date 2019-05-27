a98f3caa898d931edc5abc29d4b8e23d214439fc
山﨑 一輝 <5171028@sophia.local>
2019-05-27 09:48:23 +0900

    first commit

---
+++ practice/main.py
@@ -0,0 +1,23 @@
+import example1 as ex1
+# import example2.py
+# import example3.py
+# import example4.py
+
+ex1.member()
+print("world")
+print("hello")
+<<<<<<< HEAD
+
+=======
+print("澁谷")
+print("テスト")
+print("kamioka")
+print("pig")
+print("human")
+print("test")
+print("a")
+print("konnnichiha")
+print("sayounara")
+print("yamasaki")
+print("ino")
+>>>>>>> test
