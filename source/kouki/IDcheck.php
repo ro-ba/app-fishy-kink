8635a1f493628482d7e84a8039dfd2cb6bbb1a18
iji <root@localhost.localdomain>
2019-06-03 11:44:28 +0900

    IDチェック、passチェック変更

--- source/kouki/IDcheck.php
+++ source/kouki/IDcheck.php
@@ -1,16 +1,20 @@
 <?php
-    $mongo = new Mongo();
-    $db = $mongo->selectDB("userdb"); 
-    $collection = $db->selectCollection("userdb");
-    $id = "1a";
-    $doc = array( "_id" => "$id" ); 
-    $res = $collection->findOne( $doc ); 
-    var_dump ($res); 
+    function IDcheck($newid){
+    //$newid = $_GET['newid'];
+    $mongo = new MongoClient();
+    $db = $mongo->selectDB("User");
+    $col = new MongoCollection($db,"user");
+    
+    $check = $col -> findone(["_id" => $newid]);
+    if($check){
+        echo "NG";
+    }else{
+        echo "OK";
+    }
 
-    //idチェック
-    //if($newid){
-    //    echo "このIDは設定できません";
-    //} else {
-    //    echo "OK";
-    //}
+    }
+    if(isset($_POST[‘comment’])){
+        $comment = $_POST[‘comment’];
+        echo $comment;
+    IDcheck("1a");
 ?>
