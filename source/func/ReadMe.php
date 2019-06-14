23b7921d7f90b545bbd6fdc3cd56aa1b9cbfad5e
ro-ba <5171028@sophia.local>
2019-06-14 15:09:21 +0900

    modify ReadMe.php

--- source/func/ReadMe.php
+++ source/func/ReadMe.php
@@ -1,4 +1,5 @@
 <?php
+//Usage: FKMongo
 require "FKMongo.php";
 
 $data = connectMongo();
@@ -7,4 +8,13 @@ $data["userDB"]->insertOne(["name" => "たまの","tweetID" => "tamano"]);
 
 $a = $data["userDB"]->findOne(["name" => "たまの"]);
 print_r($a);
+
+
+//Usage:FKHash
+require "FKHash.php";
+
+$password = "P@ssword";
+$salt = "Atsushi Tamano";
+$data = fkHash($password,$salt);
+print_r($data);
 ?>
