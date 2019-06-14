aac06b6aff5468432d97b08b2ee75fc717e59466
ro-ba <5171028@sophia.local>
2019-06-14 15:11:21 +0900

    modify FKMongo.php

--- source/func/FKMongo.php
+++ source/func/FKMongo.php
@@ -8,20 +8,4 @@ function connectMongo(){
     $DMDB = $FishyKinkDB->selectCollection('DM');
     return ["userDB" => $userDB, "tweetDB" => $tweetDB, "DMDB" => $DMDB];
 }
-
-// $collection->insertOne([ 'name' => '宇治松千夜', 'shop' => '甘兎庵', 'cv' => '佐藤聡美' ]);
-// $collection->insertOne([ 'name' => '桐間紗路', 'shop' => 'フルール・ド・ラパン', 'cv' => '内田真礼']);
-// $collection->insertMany([
-//     [ 'name' => '保登心愛', 'shop' => 'ラビットハウス', 'cv' => '佐倉綾音' ],
-//     [ 'name' => '香風智乃', 'shop' => 'ラビットハウス', 'cv' => '水瀬いのり' ],
-//     [ 'name' => '天々座理世', 'shop' => 'ラビットハウス', 'cv' => '種田梨沙' ],
-// ]);
-
-// $chiya = $collection->findOne(['name' => '宇治松千夜']);
-
-// $rabbithouse = $collection->find(['shop' => 'ラビットハウス']);
-// foreach($rabbithouse as $rabbit){
-//     print_r($rabbit);
-// }
-
 ?>
