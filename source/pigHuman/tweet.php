412fa22b6d2e4223733871ef30f5159d8c870775
西林 朋樹 <5171981@sophia.local>
2019-06-17 11:42:20 +0900

    modify FKMongo.php

---
+++ source/pigHuman/tweet.php
@@ -0,0 +1,24 @@
+<?php
+require "/vagrant/source/func/FKMongo.php";
+
+$data = connectMongo();
+
+$cursor = $data["userDB"]->find(["userID"=>"tamano"]);
+//$cursor = $data["userDB"]->findOne(["userID"=>$_SESSION[‘userName’]]);
+foreach($cursor as $userData){
+    $user=$userData;
+};
+
+
+if(!empty($_POST['image'])){
+    $data["tweetDB"]->insertOne(["tweetID" => $user["userID"],"text" => $_POST['tweetText'],"image"=>$_POST['image']]);
+}
+else{
+    $data["tweetDB"]->insertOne(["tweetID" => $user["userID"],"text" => $_POST['tweetText']]);
+}
+
+//tweet($user);
+echo "実行完了";
+header( "Location: ../html/home.blade.php" ) ;
+
+?>
