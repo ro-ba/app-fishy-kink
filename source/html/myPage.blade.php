9e7e7386c199f889aa3eb6c93ad9d5414ae4c125
pigHuman <root@localhost.localdomain>
2019-06-21 15:29:44 +0900

    myPage.css 雛形

--- source/html/myPage.blade.php
+++ source/html/myPage.blade.php
@@ -8,18 +8,20 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" type="text/css" href="myPage.css">
     <link rel="shortcut icon" href="">
 </head>
 
 <body>
     <div>
-        <img name="myIcon" src="<%= icon %>" alt="myIcon" />
-        <p name="usenName">ホリカクァ（仮）</p>
-        <p name="userId">@horikakua</p>
-        <p>フォロー<span name="follow"></span>人</p>
-        <p>フォロワー<span name="follower"></span>人</p>
-        <input type="button" onclick="location.href='/setting'" value="プロフィール変更" />
+        <div class=userData>
+            <img name="myIcon" src="<%= icon %>" alt="myIcon" />
+            <p name="usenName">ホリカクァ（仮）</p>
+            <p name="userId">@horikakua</p>
+        </div>
+        <p>フォロー<span class="follow"></span>人</p>
+        <p>フォロワー<span class="follower"></span>人</p>
+        <input class="setButton" type="button" onclick="location.href='/setting'" value="プロフィール変更" />
     </div>
     <div>
         <p name="profile">プロフィール</p>
