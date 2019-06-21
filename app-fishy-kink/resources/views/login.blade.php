b317f655294865b051acfa00562e2ef56fb9c797
ro-ba <5171028@sophia.local>
2019-06-21 15:49:30 +0900

    add simple css use bootstrap

--- app-fishy-kink/resources/views/login.blade.php
+++ app-fishy-kink/resources/views/login.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
     <title>login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
@@ -19,13 +20,13 @@
     <form method="POST" action="/login">
         @csrf
         {{ csrf_field() }}
-        userID:<input type="text" name="userID"/>
-        password:<input type="password" name="password"/>
-        <input type="submit" value="login" />
+        <div class="form-group">userID:<input class="form-control" type="text" name="userID"/></div>
+        password:<input class="form-control" type="password" name="password"/></div>
+        <div class="form-group"><input class="btn btn-primary" type="submit" value="login" />
     </form>
-    <div><a href="/login">ID・パスワードを忘れた場合はこちら</a></div>
+    <div class="btn btn-warning"><a href="/login">ID・パスワードを忘れた場合はこちら</a></div>
     <div>または</div>
-    <input class="registerButton" type="button" onclick="location.href='/signUp'" value="新規登録">
+    <input class="brn btn-primary" class="registerButton" type="button" onclick="location.href='/signUp'" value="新規登録">
     
 </body>
 
