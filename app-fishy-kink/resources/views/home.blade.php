ccb5d023b225045039069a12eae33a57543c8cf6
horikawa <5171031@sophia.local>
2019-09-20 13:28:54 +0900

    サーチ

--- app-fishy-kink/resources/views/home.blade.php
+++ app-fishy-kink/resources/views/home.blade.php
@@ -14,29 +14,6 @@
 <link rel="stylesheet" href="font/css/open-iconic-bootstrap.css">
 </head>
 
-<script>
-$(function){
-    function(){
-    $.ajax( {  // ajax の非同期通信として ajaxメソッドを使用
-        // 以下Ajax のオプション指定
-        type: GET,
-        url: home.blade.php
-    })
-    $.ajaxSetup({
-        type: "POST",
-        timeout: 5000, // 10sec
-    });
-    // $.ajax()を呼び出す。
-    $.ajax({
-        url: "../home.blade.php",
-        data: null
-    });
-}
-};
-
-</script>
-
-
 <body>
     <div id="menu row d-inline col-md-12"> 
         <button type="button" class="link_button btn page-link text-dark d-inline-block" onclick="location.href='/home'">home</button>
