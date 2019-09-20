0aab39b0712cbe6d7ae952475b30ec5515433f74
komaduki <vagrant@localhost.localdomain>
2019-09-20 14:55:29 +0900

    9-20

--- app-fishy-kink/resources/views/test.blade.php
+++ app-fishy-kink/resources/views/test.blade.php
@@ -13,11 +13,11 @@
 <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
 <script>
 $(function(){ // 遅延処理
-  setInterval((function update(){ //10000ミリ秒ごとにupdateという関数を実行する
+  setInterval((function update(){ //1000ミリ秒ごとにupdateという関数を実行する
     $.ajax({
       type: 'GET',
-      url: '/api/reloadTweet', // url: は読み込むURLを表す
-      dataType: 'json', // 読み込むデータの種類を記入
+      url: '/api/reloadTweet',    // url: は読み込むURLを表す
+      dataType: 'json',           // 読み込むデータの種類を記入
       data: null,
       cache: false
       }).done(function (results) {
@@ -26,11 +26,11 @@ $(function(){ // 遅延処理
         results.forEach(function(tweet){
           // console.log(tweet);
           $('#main-contents').append('<div class="tweetTop card-header">');
-        if (tweet["type"] == "retweet"){
-          $('#main-contents').append('<div class="retweet-user">'+ tweet["userID"] + 'さんがリツイートしました</div>');
-        }
-        $('#main-contents').append('<div class="tweet-user">'+ tweet["userID"] + '</div>');
-        $('#main-contents').append('<div class="tweetMain card-body">'+ tweet["text"] + '</div>');
+          if (tweet["type"] == "retweet"){
+            $('#main-contents').append('<div class="retweet-user">'+ tweet["userID"] + 'さんがリツイートしました</div>');
+          }
+          $('#main-contents').append('<div class="tweet-user">'+ tweet["userID"] + '</div>');
+          $('#main-contents').append('<div class="tweetMain card-body">'+ tweet["text"] + '</div>');
       });
       // $('#main-contents').text(results);
       }).fail(function (err) {
@@ -40,41 +40,6 @@ $(function(){ // 遅延処理
       return update;
     }()),1000);
 });
-
-
-
-
-
-// $(function(){ // 遅延処理
-    
-//     setInterval(update, 100);
-//     //10000ミリ秒ごとにupdateという関数を実行する
-// function update(){
-//   $.ajax({
-//     type: 'GET',
-//     url: '/api/reloadTweet', // url: は読み込むURLを表す
-//     dataType: 'json', // 読み込むデータの種類を記入
-//     data: null,
-//     cache: false
-//   }).done(function (results) {
-//     // 通信成功時の処理
-//     $('#main-contents').empty();
-//     results.forEach(function(tweet){
-//       // console.log(tweet);
-//       $('#main-contents').append('<div class="tweetTop card-header">');
-//       if (tweet["type"] == "retweet"){
-//         $('#main-contents').append('<div class="retweet-user">'+ tweet["userID"] + 'さんがリツイートしました</div>');
-//       }
-//       $('#main-contents').append('<div class="tweet-user">'+ tweet["userID"] + '</div>');
-//       $('#main-contents').append('<div class="tweetMain card-body">'+ tweet["text"] + '</div>');
-//     });
-//     // $('#main-contents').text(results);
-//   }).fail(function (err) {
-//     // 通信失敗時の処理
-//     alert('ファイルの取得に失敗しました。');
-//   });
-// }
-// });
 </script>
 </head>
 <body>
@@ -83,3 +48,39 @@ $(function(){ // 遅延処理
     <div id="main-contents"><div>
 </body>
 </html>
+
+<div id="centerContents" class="col-sm-6">
+            <div class="tweet card">
+            @foreach ($tweets as $tweet)
+                <div class="tweetTop card-header">
+                @if ($tweet["type"] == "retweet")
+                    <div class="retweet-user">{{ $tweet["userID"] }}さんがリツイートしました</div>
+
+                @endif
+                <div class="tweet-user"> {{ $tweet["userID"] }} </div>
+                <div class="time"> {{ $tweet["time"] }}</div>
+                        <!-- <div class="date">{{ explode(" ",$tweet["time"])[0] }}</div> 　
+                        <div class="time">{{ explode(" ",$tweet["time"])[1] }}</div> -->
+                </div>
+                <div class="tweetMain card-body">
+                    {{ $tweet["text"] }}
+                </div>
+
+                @isset($tweet["img"][0])
+                @foreach($tweet["img"] as $img)
+                    <img src=" {{ $img }}" />
+                @endforeach
+                @endisset
+                <div class="tweetBottom d-inline">
+                    <div class="reply d-inline-block">
+                        <image src="images/reply.jpg"/>
+                    </div>
+                    <div class="retweet d-inline-block">
+                        <image src="images/retweet.png"/>
+                    </div>
+                    <div class="fab d-inline-block">
+                        <image src="images/fabo.jpg"/>
+                    </div>
+                </div>
+            @endforeach
+            </div>
