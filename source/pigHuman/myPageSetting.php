506ccf49136984847f49fa99b05e92a4c50fd8b4
iji <5171006@sophia.local>
2019-09-30 12:00:26 +0900

    プロフィール変更を変更中

--- source/pigHuman/myPageSetting.php
+++ source/pigHuman/myPageSetting.php
@@ -1,17 +1,13 @@
 <?php
 //require_once dirname(__FILE__) . '/vendor/autoload.php';
 
-function myPageSetting($id,$name,$profile){
-    $request = $request -> all();
-    //$id = session('userID');
-    // $FishyKink['userDB']->updata(array("userID"=>$id),$request);
-    $FishyKink["userDB"] -> 
-    update(array("userID" => $id),
-    array('$set' => array("userName" => $name)),
-    //array('$set' => array("userImg" => 'data:image/' . $ext . ';base64,' . $encode_img)),
-    array('$set' => array("profile" => $profile)));
+function myPageSetting($id, $request,$FishyKink){
+    $name = $request->input("userName");
+    $profile = $request->input("profileText");
+    $FishyKink["userDB"]->updateOne(["userID" => $id], ['$set'=> ["userName" => $name]]);
+    $FishyKink["userDB"]->updateOne(["userID" => $id], ['$set'=> ["profile" => $profile]]);
         
-    return "OK";
+    return "変更しました";
 }
 
 ?>
