84946df4867bd886ed1aae07f246ace2739622ea
iji <5171006@sophia.local>
2019-10-04 15:16:57 +0900

    test

--- source/pigHuman/myPageSetting.php
+++ source/pigHuman/myPageSetting.php
@@ -4,11 +4,11 @@
 function myPageSetting($id, $request,$FishyKink){
     $name = $request->input("userName");
     $profile = $request->input("profileText");
-    if(empty($name)); //userNameが空だったら
+    if(empty($name)){ //userNameが空だったら
         return "変更できませんでした。";
-    else; //空じゃなかったら変更
+    }else{ //空じゃなかったら変更
         $FishyKink["userDB"]->updateOne(["userID" => $id], ['$set'=> ["userName" => $name] , ["profile" => $profile]]);
         return "変更しました。";
+    }
 }
-
 ?>
