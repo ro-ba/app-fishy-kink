00bbf46b113dc67f96b71d51f764c7aaa151d03c
horikawaZ <5171031@sophia.local>
2019-10-11 14:21:02 +0900

    css追加

--- app-fishy-kink/resources/views/top.blade.php
+++ app-fishy-kink/resources/views/top.blade.php
@@ -1,5 +1,42 @@
-
 <!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>fishy kinkへようこそ</title>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/top.css">
+<link rel="shortcut icon" href="">
+<link href="https://fonts.googleapis.com/css?family=Caveat|Josefin+Sans|Vibes&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Dosis|Inconsolata|Josefin+Sans|Turret+Road&display=swap" rel="stylesheet">
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+</head>
+
+<body>
+  <div id="wrap">
+    <div class="content">
+
+      <div class="image"></div>
+
+      <div class="title" id="welcome area">
+          <h1>ようこそ　<span class="app-name">Fishy Kink</span>　へ</h1>
+      </div>
+
+      <div class="btn-group">
+          <button class="btn-signup" type="button"onclick="location.href='./signUp'">アカウント作成</button>
+          <button class="btn-login" type="button"onclick="location.href='/login'">ログイン</button>
+      </div>
+
+    </div>
+  </div>
+</body>
+
+</html>
+
+
+<!-- <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,15 +49,17 @@
 <link rel="shortcut icon" href="">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Caveat|Josefin+Sans|Vibes&display=swap" rel="stylesheet">
-
 </head>
+
 <body>
-    <div class="jumbotron" id="welcome area">
-        <h1>ようこそ　Fishy Kink　へ</h1>
-    </div>
-    <div id="link area">
-        <button class="btn btn-default" type="button"onclick="location.href='./signUp'">アカウント作成</button>
-        <button class="btn btn-default" type="button"onclick="location.href='/login'">ログイン</button>
+    <div class="content">
+        <div class="jumbotron" id="welcome area">
+            <h1>ようこそ　<span>Fishy Kink</span>　へ</h1>
+        </div>
+        <div id="link area">
+            <button class="btn btn-default" type="button"onclick="location.href='./signUp'">アカウント作成</button>
+            <button class="btn btn-default" type="button"onclick="location.href='/login'">ログイン</button>
+        </div>
     </div>
 </body>
-</html>
+</html> -->
