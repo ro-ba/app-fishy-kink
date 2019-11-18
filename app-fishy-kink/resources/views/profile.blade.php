6020c994cc47ff87a850a175dafde8caf84604d5
horikawaZ <5171031@sophia.local>
2019-11-18 11:01:48 +0900

    css追加

--- app-fishy-kink/resources/views/profile.blade.php
+++ app-fishy-kink/resources/views/profile.blade.php
@@ -11,7 +11,10 @@
 <link rel="shortcut icon" href="">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="font/css/open-iconic-bootstrap.css">
-<link rel="stylesheet" type="text/css" href="css/profile.css">
+<!-- <link rel="stylesheet" type="text/css" href="css/profile.css"> -->
+<link rel="stylesheet" type="text/css" href="css/profileh.css">
+<link href="https://fonts.googleapis.com/earlyaccess/kokoro.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/earlyaccess/kokoro.css" rel="stylesheet">
 <link rel="stylesheet" href="css/loader.css">
 
 <style>
@@ -37,64 +40,67 @@
 
 </head>
 <body>
-    <div id="tweet" class="tweet" style="overflow-y:scroll;"></div>
+    <!-- <div id="tweet" class="tweet" style="overflow-y:scroll;"></div> -->
 
-    <div id="tweet" class="tweet" style="height:600px; width:100%; overflow-y:scroll;"></div>
+    <!-- <div id="tweet" class="tweet" style="height:600px; width:100%; overflow-y:scroll;"></div> -->
 
 </head>
 <body>
 @isset($userData)
-  <div class = "userBar">
-      <div class="userData">
-          <img class="Images" id="myIcon" src='{{ $userData["userImg"] }}' alt="myIcon" />
-          <p id="usenName">{{ $userData["userName"] }}</p>
-          <p id="userId"><span>@</span>{{ $userData["userID"] }}</p>
+
+    <div class="profile">
+      <div id=wrap>
+
+        <div class="image">
+          <img class="myicon" id="myIcon" src='{{ $userData["userImg"] }}' alt="myIcon" />
+        </div>
+
+        <ul class="user">
+              <li class="user-name">{{ $userData["userName"] }}</li>
+            @if($isShowSettings)
+              <li class="user-edit"><input class="setButton" type="button" onclick="location.href='/settings'" value="プロフィール変更" /></li>
+            @endif
+        </ul>
+
+        <div class="user-id"><span>@</span>{{ $userData["userID"] }}</div>
+          
+        <ul class="follows">
+            @isset ($userData["follow"])
+                <li class="follow"><button type="button" onclick="location.href='/following?user={{$userData['userID'] }} '">フォロー中　<span></span>{{ count($userData["follow"]) }} 人</button></li>
+            @else
+                <li class="follow"><button type="button" onclick="location.href='/following?user={{$userData['userID'] }}'">フォロー<span></span>0人</button></li>
+            @endisset
+            
+            @isset ($userData["follower"]) 
+                <li class="follower"><button type="button" onclick="location.href='/followers?user={{$userData['userID'] }}'">フォロワー <span></span>{{ count($userData["follower"]) }} 人</button></li>
+            @else
+                <li class="follow"><button type="button" onclick="location.href='/followers?user={{$userData['userID'] }}'">フォロー<span></span>0人</button></li>
+                <li class="follower">フォロワー<span></span>0人</li>
+            @endisset
+
+            <li><button class="btn-real-dent" onclick="location.href='/'">戻る</button></li>
+        <ul>
+
       </div>
-      @isset ($userData["follow"])
-          <button type="button" onclick="location.href='/following?user={{$userData['userID'] }} '" class="follow">フォロー中　<span></span>{{ count($userData["follow"]) }} 人</button>
-      @else
-          <button type="button" onclick="location.href='/following?user={{$userData['userID'] }}'">フォロー<span class="follow"></span>0人</button>
-      @endisset
-      
-      @isset ($userData["follower"]) 
-          <button type="button" onclick="location.href='/followers?user={{$userData['userID'] }}'" class="follower">フォロワー <span></span>{{ count($userData["follower"]) }} 人</button>
-      @else
-          <button type="button" onclick="location.href='/followers?user={{$userData['userID'] }}'">フォロー<span class="follower"></span>0人</button>
-          <p class="follower">フォロワー<span></span>0人</p>
-      @endisset
 
-      @if($isShowSettings)
-        <input class="setButton" type="button" onclick="location.href='/settings'" value="プロフィール変更" />
-      @endif
+    <div class="my-profile"> 
+      <p class="pro-content">{{ $userData["profile"] }}</p>    
+    </div>
 
-  <hr class="bar1"/>
-  <hr class="bar2"/>
-  <!-- <hr class="bar3"/>
-  <hr class="h1">  -->
+    <hr />
 
-    <button class="btn-real-dent" onclick="location.href='/'">戻る
-    <i class="fa fa=home"></i>
-    </button>
+    <div class="loader"></div>
 
-     
-</a>
-
-
+    <div class="row tweets">
+      <div id="leftContents" class="col-sm-3"></div>
+      <div id="centerContents" class="col-sm-6"></div>
+      <div id="rightContents" class="col-sm-3"></div>
+    </div>       
+  @else
+    <b>ユーザーが存在しません。</b>
+    <button onclick="location.href='/'">戻る</button>
+  @endisset
   </div>
-  <div class="profile">
-    <p>プロフィール</p>
-        <p>{{ $userData["profile"] }}</p>    
-  </div>      
-  <div class="loader">Loading...</div>
-  <div class="row tweets">
-    <div id="leftContents" class="col-sm-3"></div>
-    <div id="centerContents" class="col-sm-6"></div>
-    <div id="rightContents" class="col-sm-3"></div>
-  </div>       
-@else
-  <b>ユーザーが存在しません。</b>
-  <button onclick="location.href='/'">戻る</button>
-@endisset
-</body>
 
+</body>
 </html>
