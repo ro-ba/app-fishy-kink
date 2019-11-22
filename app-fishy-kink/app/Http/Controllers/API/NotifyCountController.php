bfeb3afcddb1259a149aec839f9b19c6e52a4a7a
pigHuman <5171981@st.hsc.ac.jp>
2019-11-22 14:16:24 +0900

    notify modify

--- app-fishy-kink/app/Http/Controllers/API/NotifyCountController.php
+++ app-fishy-kink/app/Http/Controllers/API/NotifyCountController.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-require "/vagrant/source/func/FKSession.php";
-require "/vagrant/source/func/FKMongo.php";
-
-class NotifyController extends Controller
+class NotifyCountController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,25 +13,6 @@ class NotifyController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        $id = session("userID");
-        $FishyKink = connect_mongo();
-
-        $notifyList = $FishyKink["notifyDB"] -> find(["userID" => $id]);
-        $count = $FishyKink["notifyDB"] -> count(["userID" => $id , "readFlag" => false]);
-        $FishyKink["notifyDB"] -> updateMany(["userID" => $id],['$set' => ["readFlag" => true]]);
-
-        $userData = $FishyKink["userDB"] -> findOne(["userID" => $id]);
-
-        return view("notify",compact("userData","notifyList","count"));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
     {
         //
     }
@@ -46,7 +25,11 @@ class NotifyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userID = session('userID');
+        $db = connect_mongo();
+        $return = $db["notifyDB"] -> count(["userID" => $userID , "readFlag" => false]);
+        
+        return ["message" => $return];
     }
 
     /**
@@ -56,17 +39,6 @@ class NotifyController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
